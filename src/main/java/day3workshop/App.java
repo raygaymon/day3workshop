72e2af6ce66b6a50ba429f31b1beb339621739a9
raygaymon <raysonlim.jr@gmail.com>
2023-04-19 16:19:41 +0800

    first commit

---
+++ src/main/java/day3workshop/App.java
@@ -0,0 +1,13 @@
+package day3workshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
